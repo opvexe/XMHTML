b9a1204ab3f0b88452e5d3a14c1f6ce1b82430be
shumintao <tao_shuminX@foxmail.com>
2017-10-10 17:17:03 +0800

    no message

--- ExperimentApp/Class/Utils/View/DrawerView/EX_leftHeadView.m
+++ ExperimentApp/Class/Utils/View/DrawerView/EX_leftHeadView.m
@@ -19,10 +19,22 @@
     if (self) {
         self.backgroundColor = [UIColor brownColor];
         
+        
         _cutButton = [UIButton buttonWithType:UIButtonTypeCustom];
         [_cutButton.titleLabel setFont:[UIFont systemFontOfSize:12.0]];
         [_cutButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [_cutButton setTitle:@"切换科目" forState:UIControlStateNormal];
+        [_cutButton setImage:[UIImage imageNamed:@"my_icon_enter_n"] forState:UIControlStateNormal];
+        
+        CGFloat imageWidth = 20;
+        CGFloat imageHeight = 20;
+        CGFloat labelWidth = [_cutButton.titleLabel.text sizeWithFont:_cutButton.titleLabel.font].width;
+        CGFloat labelHeight = [_cutButton.titleLabel.text sizeWithFont:_cutButton.titleLabel.font].height;
+        
+        _cutButton.imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth, 0, -(labelWidth));
+        _cutButton.titleEdgeInsets = UIEdgeInsetsMake(0, -imageWidth, 0, imageWidth);
+       //top, left, bottom, right 如果同时有image和label，那这时候image的上左下是相对于button，右边是相对于label的；title的上右下是相对于button，左边是相对于image的。
+        
         [_cutButton addTarget:self action:@selector(cut:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:self.cutButton];
         
@@ -30,7 +42,7 @@
         [self addSubview:self.titleLabel];
         
         [_cutButton mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.right.mas_equalTo(self).mas_offset(-30.0);
+            make.right.mas_equalTo(self).mas_offset(-20.0);
             make.top.mas_equalTo(self).mas_equalTo(40.0);
         }];
         
